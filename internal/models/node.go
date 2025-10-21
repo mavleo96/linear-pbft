8c4108115d2d0a8824fba1a101aef347c7832f55
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-10-21 01:41:51 -0400

    add client model and store public key and client in node and client models

--- internal/models/node.go
+++ internal/models/node.go
@@ -1,13 +1,83 @@
 package models
 
+import (
+	"path/filepath"
+
+	"github.com/mavleo96/bft-mavleo96/internal/config"
+	"github.com/mavleo96/bft-mavleo96/internal/security"
+	"github.com/mavleo96/bft-mavleo96/internal/utils"
+	"github.com/mavleo96/bft-mavleo96/pb"
+)
+
 // Node represents a node in the distributed system
 type Node struct {
 	ID        string `yaml:"id"`
 	Address   string `yaml:"address"`
 	PublicKey []byte
+	Client    *pb.LinearPBFTNodeClient
+	Close     func() error // TODO: this needs to be changed
 }
 
-// String returns a string representation of the node
-func (n *Node) String() string {
-	return n.ID
+type Client struct {
+	ID        string `yaml:"id"`
+	Address   string `yaml:"address"`
+	PublicKey []byte
+	Client    *pb.LinearPBFTClientAppClient
+	Close     func() error
+}
+
+func GetNodeMap(nodeConfig map[string]*config.NodeEntry) (map[string]*Node, error) {
+	nodeMap := make(map[string]*Node)
+	for id, node := range nodeConfig {
+		// Read and store public key
+		publicKey, err := security.ReadPublicKey(filepath.Join("./keys", "node", id+".pub.pem"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Connect to node
+		conn, err := utils.Connect(node.Address)
+		if err != nil {
+			return nil, err
+		}
+		nodeClient := pb.NewLinearPBFTNodeClient(conn)
+
+		// Create node struct and store in map
+		nodeMap[id] = &Node{
+			ID:        node.ID,
+			Address:   node.Address,
+			PublicKey: publicKey,
+			Client:    &nodeClient,
+			Close:     func() error { return conn.Close() },
+		}
+	}
+	return nodeMap, nil
+}
+
+func GetClientMap(clientConfig map[string]*config.ClientEntry) (map[string]*Client, error) {
+	clientMap := make(map[string]*Client)
+	for id, client := range clientConfig {
+		// Read and store public key
+		publicKey, err := security.ReadPublicKey(filepath.Join("./keys", "client", id+".pub.pem"))
+		if err != nil {
+			return nil, err
+		}
+
+		// Connect to client
+		conn, err := utils.Connect(client.Address)
+		if err != nil {
+			return nil, err
+		}
+		clientClient := pb.NewLinearPBFTClientAppClient(conn)
+
+		// Create client struct and store in map
+		clientMap[id] = &Client{
+			ID:        client.ID,
+			Address:   client.Address,
+			PublicKey: publicKey,
+			Client:    &clientClient,
+			Close:     func() error { return conn.Close() },
+		}
+	}
+	return clientMap, nil
 }
