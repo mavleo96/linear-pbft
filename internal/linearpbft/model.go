9f76c6c7b3bb03d462e4c1cf92e42d434d58ad6d
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-10-21 01:42:10 -0400

    add placeholder readonly method

--- internal/linearpbft/model.go
+++ internal/linearpbft/model.go
@@ -1,14 +1,55 @@
 package linearpbft
 
 import (
+	"context"
+
 	"github.com/mavleo96/bft-mavleo96/internal/database"
 	"github.com/mavleo96/bft-mavleo96/internal/models"
+	"github.com/mavleo96/bft-mavleo96/internal/security"
+	"github.com/mavleo96/bft-mavleo96/internal/utils"
 	"github.com/mavleo96/bft-mavleo96/pb"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-// LinearPBFT represents a LinearPBFT node
-type LinearPBFTServer struct {
+// LinearPBFTNode represents a LinearPBFT node
+type LinearPBFTNode struct {
 	*models.Node
-	DB *database.Database
-	*pb.UnimplementedLinearPBFTServer
+	PrivateKey []byte
+	DB         *database.Database
+	Peers      map[string]*models.Node
+	Clients    map[string]*models.Client
+	*pb.UnimplementedLinearPBFTNodeServer
+}
+
+func (n *LinearPBFTNode) ReadOnly(ctx context.Context, req *pb.TransactionRequest) (*emptypb.Empty, error) {
+
+	// Verify signature
+	ok := security.Verify(utils.MessageString(req), n.Clients[req.Sender].PublicKey, req.Signature)
+	if !ok {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid signature")
+	}
+
+	// Process transaction
+	balance, err := n.DB.GetBalance(req.Transaction.Sender)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response := &pb.TransactionResponse{
+		ViewNumber: 0,
+		Timestamp:  req.Timestamp,
+		Sender:     req.Sender,
+		NodeID:     n.ID,
+		Result:     int64(balance),
+	}
+	response.Signature = security.Sign(utils.MessageString(response), n.PrivateKey)
+
+	// Send reply to client
+	_, err = (*n.Clients[req.Sender].Client).ReceiveReply(ctx, response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &emptypb.Empty{}, nil
 }
