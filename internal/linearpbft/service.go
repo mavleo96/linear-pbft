4fa269c2957cb816af94b344a8e7e54ce70b60ed
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-10-23 21:32:41 -0400

    separate code in model.go to service and state

--- internal/linearpbft/service.go
+++ internal/linearpbft/service.go
@@ -2,10 +2,7 @@ package linearpbft
 
 import (
 	"context"
-	"sync"
 
-	"github.com/mavleo96/bft-mavleo96/internal/database"
-	"github.com/mavleo96/bft-mavleo96/internal/models"
 	"github.com/mavleo96/bft-mavleo96/internal/security"
 	"github.com/mavleo96/bft-mavleo96/pb"
 	log "github.com/sirupsen/logrus"
@@ -14,25 +11,7 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-// LinearPBFTNode represents a LinearPBFT node
-type LinearPBFTNode struct {
-	*models.Node
-	PrivateKey []byte
-	DB         *database.Database
-
-	Peers   map[string]*models.Node
-	F       int
-	N       int
-	Clients map[string]*models.Client
-
-	Mutex         sync.Mutex
-	ViewNumber    int64
-	PrePrepareLog map[int64]*pb.PrePrepareMessage
-	PrepareLog    map[int64]*pb.PrepareMessage
-
-	*pb.UnimplementedLinearPBFTNodeServer
-}
-
+// TransferRequest handles incoming transaction requests from clients
 func (n *LinearPBFTNode) TransferRequest(ctx context.Context, signedRequest *pb.SignedTransactionRequest) (*emptypb.Empty, error) {
 	request := signedRequest.Request
 
---
+++ internal/linearpbft/state.go
@@ -0,0 +1,28 @@
+package linearpbft
+
+import (
+	"sync"
+
+	"github.com/mavleo96/bft-mavleo96/internal/database"
+	"github.com/mavleo96/bft-mavleo96/internal/models"
+	"github.com/mavleo96/bft-mavleo96/pb"
+)
+
+// LinearPBFTNode represents a LinearPBFT node
+type LinearPBFTNode struct {
+	*models.Node
+	PrivateKey []byte
+	DB         *database.Database
+
+	Peers   map[string]*models.Node
+	F       int
+	N       int
+	Clients map[string]*models.Client
+
+	Mutex         sync.Mutex
+	ViewNumber    int64
+	PrePrepareLog map[int64]*pb.PrePrepareMessage
+	PrepareLog    map[int64]*pb.PrepareMessage
+
+	*pb.UnimplementedLinearPBFTNodeServer
+}
